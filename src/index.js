6bdcd9a084e7a0e05f6230d2d432cabe14a37c1c
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-07-23 19:38:54 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -5,8 +5,5 @@ import './js/modal-order-now';
 import './js/header-mobile-menu';
 import './js/js-popular';
 import './js/js-categories';
-<<<<<<< HEAD
-=======
 import './js/favorites-page';
->>>>>>> main
 import './js/hero';
