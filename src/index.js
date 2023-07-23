6fcbb816aec0976c03200b0989969be981aae263
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-07-23 19:36:51 +0300

    Merge branch 'main' into hero-html

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import './js/modal-order-now';
 import './js/header-mobile-menu';
 import './js/js-popular';
 import './js/js-categories';
+<<<<<<< HEAD
+=======
+import './js/favorites-page';
+>>>>>>> main
+import './js/hero';
