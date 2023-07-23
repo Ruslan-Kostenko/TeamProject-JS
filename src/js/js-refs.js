3a6f037b2038514e722d4283b6f24d7401aca4e3
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-07-23 04:25:08 +0300

    fix

--- src/js/js-refs.js
+++ src/js/js-refs.js
@@ -1,9 +1,9 @@
 export const refs = {
-//   HEADER
+  // HEADER
   mobileMenu: document.querySelector('.js-menu-container'),
   openMenuBtn: document.querySelector('.js-open-menu'),
   closeMenuBtn: document.querySelector('.js-close-menu'),
-//   CATEGORIES&POPULAR
-    ulPopEl:document.querySelector('.pop-list'),
-    ulCatEl:document.querySelector('.cat-list'),
+  // CATEGORIES&POPULAR
+  ulPopEl: document.querySelector('.pop-list'),
+  ulCatEl: document.querySelector('.cat-list'),
 };
