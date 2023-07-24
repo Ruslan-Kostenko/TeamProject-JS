fd376413a801590253b997386416e777cf706ae1
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-07-24 22:13:24 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './js/js-refs';
 import './js/scroll-to-up';
-import './js/modal-order-now';
 
-import './js/header-mobile-menu';
 import './js/hero';
 import './js/js-popular';
 import './js/js-categories';
 import './js/favorites-page';
+
+import './js/header-mobile-menu';
+import './js/modal-order-now';
