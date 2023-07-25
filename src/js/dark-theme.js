289afcf732aa0d025a2abac3e542691aff05b217
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-07-25 17:59:23 +0300

    dark-theme & fixing

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const allBodyTheme = document.body;
   const themeSwitcher = document.querySelector('.switcher-toggle.desktop');
+
   const mobileThemeSwitcher = document.querySelector('.switcher-toggle.mobile');
   //   const desktopHeader = document.querySelector('.head-theme');
 
@@ -26,6 +27,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  const handleSetTheme = () => {
+    const storageTheme = localStorage.getItem('theme-point');
+    if (storageTheme === 'dark') {
+      themeSwitcher.checked = true;
+      mobileThemeSwitcher.checked = true;
+      allBodyTheme.classList.add('dark-theme');
+    } else {
+      themeSwitcher.checked = false;
+      mobileThemeSwitcher.checked = false;
+      allBodyTheme.classList.remove('dark-theme');
+    }
+  };
+
   themeSwitcher.addEventListener('change', handleThemeSwitcher);
+  // themeFavSwitcher.addEventListener('change', handleFavThemeSwitcher);
   mobileThemeSwitcher.addEventListener('change', handleMobileThemeSwitcher);
+
+  handleSetTheme();
+
+  window.addEventListener('pageshow', () => {
+    handleSetTheme();
+  });
 });
