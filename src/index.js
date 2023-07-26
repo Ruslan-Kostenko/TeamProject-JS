0498a215cfe4c3da2a7d9ed5cfec019f0f3c5830
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-07-26 11:56:29 +0300

    хххх

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import './js/dark-theme';
 import './js/scroll-to-up';
 import './js/header-mobile-menu';
-// import './js/popular-recipe';
+import './js/popular-recipe';
 // import './js/modal-order-now';
 // import './js/header-mobile-menu';
 
