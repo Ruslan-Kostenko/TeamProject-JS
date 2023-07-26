d37634b5ad9314edf1c2f2834be4755739378384
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-07-26 12:59:43 +0300

    pop-up recipe

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import './js/dark-theme';
 import './js/scroll-to-up';
 import './js/header-mobile-menu';
 import './js/popular-recipe';
-// import './js/modal-order-now';
-// import './js/header-mobile-menu';
 
 import './js/hero';
 import './js/js-categories';
