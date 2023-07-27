63fc64a30e803e05e8b00e266221d83af3823ed2
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-07-27 15:38:38 +0300

    remakepopular

--- src/js/js-popular.js
+++ src/js/js-popular.js
@@ -2,6 +2,25 @@ const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/';
 const ulPopEl = document.querySelector('.pop-list');
 fetchPopItem().then(data => {
   ulPopEl.innerHTML = makePopMarckap(data);
+  ulPopEl.addEventListener('click', evt => {
+    const popImgLink = evt.target.closest('.pop-img-link');
+    if (!popImgLink) {
+      return;
+    }
+    const seeRecipe = popImgLink.querySelectorAll('.recipe_desc_btn');
+    onClickRecipeDescrBtn();
+  
+    function onClickRecipeDescrBtn() {
+      const popUpRecipe = document.querySelector('.pop-up-recipe');
+      const backdropPopupRecipe = document.querySelector(
+        '.backdrop-popup-recipe'
+      );
+  
+      popUpRecipe.style.display = 'block';
+      backdropPopupRecipe.style.display = 'block';
+    }
+  
+})
 });
 function fetchPopItem() {
   return fetch(BASE_URL + 'recipes/popular').then(resp => {
@@ -16,14 +35,14 @@ function makePopMarckap(arr) {
     .map(
       c =>
         `<li class="pop-item">
-          <a href="#" class="pop-img-link">
+          <button value="${c._id}" class="pop-img-link">
             <img class="pop-img" src="${c.preview}" alt="a">
-          </a>
-          
           <div class="pop-info">
             <h3>${c.title}</h3>
             <p>${c.description}</p>
-          </div>`
+          </div>
+          </button>
+          </li>`
     )
     .join('');
 }
